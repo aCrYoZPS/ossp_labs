6294be5fe036132210d308ef6019d15ac42e2ec4
aCrYoZ <acryozps@gmail.com>
2026-02-27 22:46:17 +0300

    lab2

---
+++ sem6/lab2/main.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Parse options
+OPTION=""
+if [[ "$1" == -* && "$1" != "-h" && "$1" != "--help" ]]; then
+    OPTION="$1"
+    shift
+fi
+
+if [[ "$1" == "-h" || "$1" == "--help" || $# -lt 1 ]]; then
+    echo "Usage: $0 [option] [file1] [file2] ..."
+    echo "Options: -n (name), -q (quantity), -t (total price)"
+    exit 0
+fi
+
+printf "%-20s | %10s | %10s | %12s\n" "Name" "Quantity" "Avg. price" "Total"
+echo "------------------------------------------------------------"
+
+awk -F ',[[:space:]]*' '
+BEGIN {
+    format = "%-20s | %10.2f | %10.2f | %12.2f\n"
+}
+{
+    if (NF < 3) next;
+    name = $1; qty = $2; price = $3
+    count[name] += qty
+    total[name] += (qty * price)
+}
+END {
+    for (i in count) {
+        printf format, i, count[i], total[i]/count[i], total[i]
+    }
+}' "$@" | {
+    case "$OPTION" in
+        -n) sort -t'|' -k1,1 ;;
+        -q) sort -t'|' -k2,2rn ;;
+        -t) sort -t'|' -k4,4rn ;;
+        *)  cat ;;
+    esac
+}
